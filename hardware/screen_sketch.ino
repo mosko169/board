576654063609878d862095be54db1975d8ec9b28
yarden.moskovich <mosko169@gmail.com>
2019-05-14 00:43:00 +0300

    touch screen sketch

---
+++ hardware/screen_sketch.ino
@@ -0,0 +1,61 @@
+/*=================================
+This code demostrates 4-Wire Touch screen 
+interfacing with Arduino
+
+blog.circuits4you.com
+www.circuits4you.com
+
+4- Wire Touchscreen Connections
+A0=====X+
+A1=====X-
+A2=====Y+
+A3=====Y-
+=================================*/
+//Define your Touch screen connections
+#define X1 A0
+#define X2 A1 
+#define Y1 A2   
+#define Y2 A3 
+
+#define MIN_X 50
+#define MAX_X 900
+
+#define MIN_Y 120
+#define MAX_Y 900
+
+void setup()
+{
+   Serial.begin(9600);
+}
+
+void loop()
+{
+   int X,Y; //Touch Coordinates are stored in X,Y variable
+   pinMode(Y1,INPUT);
+   pinMode(Y2,INPUT);  
+   digitalWrite(Y2,LOW);
+   pinMode(X1,OUTPUT);
+   digitalWrite(X1,HIGH);
+   pinMode(X2,OUTPUT);
+   digitalWrite(X2,LOW);
+   delayMicroseconds(100);
+   X = ((float)(analogRead(Y1) - MIN_X) / (MAX_X - MIN_X)) * 100;
+
+   
+   pinMode(X1,INPUT);
+   pinMode(X2,INPUT);
+   digitalWrite(X2,LOW);
+   pinMode(Y1,OUTPUT);
+   digitalWrite(Y1,HIGH);
+   pinMode(Y2,OUTPUT);
+   digitalWrite(Y2,LOW);
+   delayMicroseconds(100);
+   Y = ((float)(analogRead(X1) - MIN_Y) / (MAX_Y - MIN_Y)) * 100;
+      
+  //Display X and Y on Serial Monitor
+   Serial.print("X = ");  
+   Serial.println(X);
+   Serial.print("Y = ");
+   Serial.println(Y);
+   delay(200);
+}
